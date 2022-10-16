8cd21ac652f8949c8c17cc8b9bba0bed32df9bee
Mohammad Arif <arif_iq@yahoo.co.in>
2022-10-16 22:17:15 +0530

    Initial commit

---
+++ src/error-page.jsx
@@ -0,0 +1,21 @@
+import { useRouteError } from 'react-router-dom';
+
+export default function ErrorPage() {
+  /**
+   * useRouteError()
+   *
+   * Inside of an errorElement, this hooks returns anything
+   * thrown during an action, loader, or rendering.
+   */
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <div id='error-page'>
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
