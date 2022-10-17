9df3db32f90b52721c9ef538a9b0d902ffcd1ee1
Mohammad Arif <arif_iq@yahoo.co.in>
2022-10-17 10:43:15 +0530

    Opimistic UI

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -68,6 +68,10 @@ function Favorite({ contact }) {
   const fetcher = useFetcher();
   // yes, this is a `let` for later
   let favorite = contact.favorite;
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get('favorite') === 'true';
+  }
+
   return (
     <fetcher.Form method='post'>
       <button
